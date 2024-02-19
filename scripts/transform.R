442c93b6fcd2943699dde596a9bd4cc9cdff04c9
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-02-19 14:59:32 -0300

    script funcional para forma declarativa da link table (#14)

--- scripts/transform.R
+++ scripts/transform.R
@@ -2,39 +2,34 @@ library(frictionless)
 library(data.table)
 library(relatorios)
 library(spreadmart)
+library(purrr)
+library(glue)
+library(rrapply)
 
-sisor <- suppressMessages(read_package("datapackages/sisor/datapackage.json"))
-loa_rec <- read_resource(sisor, "base_orcam_receita_fiscal") |> as.data.table()
-loa_rec$ANO <- 2024
-loa_rec$MDE  <- is_mde_rec(loa_rec)
+create_fact_table_ <- function(resource_name, package_name, config) {
+  datapackage <- read_package(glue("datapackages/{package_name}/datapackage.json"))
+  resource <- read_resource(datapackage, resource_name) |> as.data.table()
+  key_name <- config$packages[[package_name]][[resource_name]]
+  create_fact_table(resource, config$keys[key_name])
+}
 
-loa_desp <- read_resource(sisor, "base_orcam_despesa_item_fiscal") |> as.data.table()
-loa_desp$ANO <- 2024
-loa_desp$MDE <- is_mde_desp(loa_desp)
+create_fact_tables <- function(package_name, config) {
+  map(names(config$packages[[package_name]]), create_fact_table_, package_name, config)  
+}
 
+config <- RcppTOML::parseTOML("relationships.toml")
 
-reest <- suppressMessages(read_package("datapackages/reest/datapackage.json"))
-reest_rec <- read_resource(reest, "reest_rec") |> as.data.table()
-reest_rec$MDE <- is_mde_rec(reest_rec)
-reest_desp <- read_resource(reest, "reest_desp") |> as.data.table()
-reest_desp$MDE <- is_mde_desp(reest_desp)
+resource_names <- map(config$packages, names) |> unlist() |> unname()
 
+fact_tables <- map(names(config$packages), create_fact_tables, config) |> 
+               rrapply::rrapply(classes = "data.frame", how = "flatten") |> 
+               set_names(resource_names)
 
-chave_rec <- c("ANO", "UO_COD", "FONTE_COD", "MDE")
-chave_desp <- c("ANO", "UO_COD", "FUNCAO_COD", "ACAO_COD", 
-                "GRUPO_COD", "FONTE_COD", "IPU_COD", "MDE")
+resources <- map(names(config$packages), \(package_name) read_datapackage(glue("datapackages/{package_name}/datapackage.json"))) |> 
+              rrapply::rrapply(classes = "data.frame", how = "flatten")
 
-fact_loa_rec <- create_fact_table(loa_rec, list(chave_rec = chave_rec))
-fwrite(fact_loa_rec, "data/fact_loa_rec.csv")
+link_table <- create_link_table(resources, keys = config$keys)
 
-fact_reest_rec <- create_fact_table(reest_rec, list(chave_rec = chave_rec))
-fwrite(fact_reest_rec, "data/fact_reest_rec.csv")
+fact_tables |> imap(\(value, key) fwrite(value, glue("data/fact_{key}.csv")))
 
-fact_loa_desp <- create_fact_table(loa_desp, list(chave_desp = chave_desp))
-fwrite(fact_loa_desp, "data/fact_loa_desp.csv")
-
-fact_reest_desp <- create_fact_table(reest_desp, list(chave_desp = chave_desp))
-fwrite(fact_reest_desp, "data/fact_reest_desp.csv")
-
-link_table <- create_link_table(loa_rec, loa_desp, reest_rec, reest_desp, keys = list(chave_rec = chave_rec, chave_desp = chave_desp))
 fwrite(link_table, "data/link_table.csv")
